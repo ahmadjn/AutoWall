a2ed7f9d404d16e499c9a627b755ab8794daa0a0
ahmadjn <ahmadjn01@gmail.com>
2022-07-22 01:12:48 +0800

    up

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SITE_URL', 'https://AutoWall.test'); // REMOVE SLASH(/) AT THE END OF URL
-define('SITE_NAME', 'Free Download Images');
-define('SITE_DESC', 'Beautiful, free images, pictures and photos that you can download for free and use for any project.');
-define('THEME_NAME', 'default');
